6615b8a4c97e7b18382d7dc9850852a003a037f1
Bobby Nikolov <pandafreakbg@gmail.com>
2016-04-05 20:51:01 +0300

    Finished interface

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
 public class Main {
 
 	private JFrame frame;
+	private ButtonGroup bg = new ButtonGroup();
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -52,7 +61,6 @@ public class Main {
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				JOptionPane.showMessageDialog(frame, "Има \"Изход\" бутон бе!");
-				;
 			}
 		});
 
@@ -83,5 +91,38 @@ public class Main {
 			}
 		});
 		toolBar.add(button_3);
+
+		JRadioButton btnFaster = new JRadioButton("\u0417\u0430\u0431\u044A\u0440\u0437\u0430\u0439");
+		btnFaster.setBounds(10, 25, 94, 23);
+		frame.getContentPane().add(btnFaster);
+
+		JRadioButton btnSlower = new JRadioButton("\u0417\u0430\u0431\u0430\u0432\u0438");
+		btnSlower.setBounds(106, 25, 83, 23);
+		frame.getContentPane().add(btnSlower);
+
+		bg.add(btnFaster);
+		bg.add(btnSlower);
+
+		JLabel lblSubtitleScreen = new JLabel("test");
+		lblSubtitleScreen.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSubtitleScreen.setForeground(Color.WHITE);
+		lblSubtitleScreen.setBackground(Color.DARK_GRAY);
+		lblSubtitleScreen.setBounds(0, 55, 434, 29);
+		lblSubtitleScreen.setOpaque(true);
+		frame.getContentPane().add(lblSubtitleScreen);
+
+		JCheckBox chckbxRemoveTags = new JCheckBox(
+				"\u041F\u0440\u0435\u043C\u0430\u0445\u043D\u0438 \u0442\u0430\u0433\u043E\u0432\u0435\u0442\u0435");
+		chckbxRemoveTags.setBounds(286, 25, 142, 23);
+		frame.getContentPane().add(chckbxRemoveTags);
+
+		textField = new JTextField();
+		textField.setBounds(193, 26, 50, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JLabel label = new JLabel("\u043C\u043B/\u0441.");
+		label.setBounds(245, 29, 46, 14);
+		frame.getContentPane().add(label);
 	}
 }
