c84a5cb6df6722c59b9f32ca7dbd7f6c0632e5b7
Antoni Dobrenov <onesum977@gmail.com>
2016-04-09 11:51:29 +0300

    no message

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -19,15 +20,14 @@ import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Main {
 
 	private JFrame frame;
 	private ButtonGroup bg = new ButtonGroup();
-	private JTextField tfMlSeconds;
+	private JTextField textField;
 	private JLabel lblLoadedFile;
-	private JTextField tfScreen;
-	private File GlobalFile;
 
 	/**
 	 * Launch the application.
@@ -82,7 +82,6 @@ public class Main {
 		btnLoad.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
-<<<<<<< HEAD
 				// making browse window
 				JFileChooser chooser = new JFileChooser();
 				chooser.showOpenDialog(null);
@@ -93,14 +92,6 @@ public class Main {
 				} else {
 					lblLoadedFile.setText("Зареденият файл е : " + f.getName());
 				}
-=======
-				// Making browse window
-				JFileChooser chooser = new JFileChooser();
-				chooser.showOpenDialog(null);
-				File f = chooser.getSelectedFile();
-				lblLoadedFile.setText("Зареденият файл е : " + f.getName());
-				//
->>>>>>> 30678fb4e7284d382429f0eb3e16933a62de19ba
 
 			}
 		});
@@ -140,15 +131,23 @@ public class Main {
 		bg.add(btnFaster);
 		bg.add(btnSlower);
 
+		JLabel lblSubtitleScreen = new JLabel("test");
+		lblSubtitleScreen.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSubtitleScreen.setForeground(Color.WHITE);
+		lblSubtitleScreen.setBackground(Color.DARK_GRAY);
+		lblSubtitleScreen.setBounds(0, 55, 444, 29);
+		lblSubtitleScreen.setOpaque(true);
+		frame.getContentPane().add(lblSubtitleScreen);
+
 		JCheckBox chckbxRemoveTags = new JCheckBox(
 				"\u041F\u0440\u0435\u043C\u0430\u0445\u043D\u0438 \u0442\u0430\u0433\u043E\u0432\u0435\u0442\u0435");
 		chckbxRemoveTags.setBounds(286, 25, 142, 23);
 		frame.getContentPane().add(chckbxRemoveTags);
 
-		tfMlSeconds = new JTextField();
-		tfMlSeconds.setBounds(193, 26, 50, 20);
-		frame.getContentPane().add(tfMlSeconds);
-		tfMlSeconds.setColumns(10);
+		textField = new JTextField();
+		textField.setBounds(193, 26, 50, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 
 		JLabel label = new JLabel("\u043C\u043B/\u0441.");
 		label.setBounds(245, 29, 46, 14);
@@ -166,23 +165,5 @@ public class Main {
 		lbltime.setBackground(Color.GRAY);
 		lbltime.setBounds(120, 83, 201, 35);
 		frame.getContentPane().add(lbltime);
-
-		tfScreen = new JTextField();
-		tfScreen.setBounds(0, 55, 444, 29);
-		frame.getContentPane().add(tfScreen);
-		tfScreen.setColumns(10);
-
-		JButton btnPrevious = new JButton("\u041F\u0440\u0435\u0434\u0438\u0448\u0435\u043D");
-		btnPrevious.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-
-			}
-		});
-		btnPrevious.setBounds(10, 94, 100, 23);
-		frame.getContentPane().add(btnPrevious);
-
-		JButton btnNext = new JButton("\u0421\u043B\u0435\u0434\u0432\u0430\u0449");
-		btnNext.setBounds(331, 94, 103, 23);
-		frame.getContentPane().add(btnNext);
 	}
 }
