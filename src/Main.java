7927ee109f02f734ebf4613b127f8aa0697f27ae
Bobby Nikolov <pandafreakbg@gmail.com>
2016-04-16 20:07:51 +0300

    Fixed my code

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.util.Scanner;
+import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,10 +21,6 @@ import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
-import javax.swing.JTextArea;
-import javax.swing.JPanel;
-import java.awt.Panel;
-import java.awt.SystemColor;
 
 public class Main {
 
@@ -36,14 +28,16 @@ public class Main {
 	private ButtonGroup bg = new ButtonGroup();
 	private JTextField tfMlSeconds;
 	private JLabel lblLoadedFile;
-	private JTextField tfScreen;
-	public static File GlobalFile;
-	File temp;
+	private JLabel lbltime;
+	private File GlobalFile;
+	private Sub subtitle;
+	private List<String> entries;
+	private int Id;
+	private JEditorPane epScreen;
 
 	/**
 	 * Launch the application.
 	 */
-
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -65,10 +59,21 @@ public class Main {
 		initialize();
 	}
 
+	public void screen() {
+		String[] attributes = entries.get(Id).split("\n");
+		String text = "";
+		for (int i = 2; i < attributes.length; i++) {
+			text += attributes[i] + "\n";
+		}
+		epScreen.setText(text);
+		lbltime.setText(attributes[1].substring(0, 12) + " до " + attributes[1].substring(18, attributes[1].length()));
+	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		Id = 0;
 		frame = new JFrame();
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("resources\\icon.png"));
 		frame.setResizable(false);
@@ -84,110 +89,27 @@ public class Main {
 			}
 		});
 
-		JToolBar toolBar = new JToolBar();
-		toolBar.setFloatable(false);
-		toolBar.setBounds(0, 0, 434, 23);
-		toolBar.setBackground(UIManager.getColor("Button.background"));
-		frame.getContentPane().add(toolBar);
-
-		JButton btnLoad = new JButton("\u0417\u0430\u0440\u0435\u0434\u0438 \u0424\u0430\u0439\u043B");
-		btnLoad.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent arg0) {
-
-				// making browse window
-				JFileChooser chooser = new JFileChooser();
-				chooser.setDialogTitle("Load File");
-				chooser.showOpenDialog(null);
-				GlobalFile = chooser.getSelectedFile();
-
-				Scanner fileReader;
-				try {
-					fileReader = new Scanner(GlobalFile);
-					temp = new File("temp");
-					PrintStream fw = new PrintStream(temp);
-					while (fileReader.hasNextLine()) {
-						fw.println(fileReader.nextLine());
-
-					}			
-					fw.close();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-
-				String name = GlobalFile.getName();
-				if (name.endsWith(".srt") == false && name.endsWith(".sub") == false) {
-					JOptionPane.showMessageDialog(null, "Моля изберете \".srt или .sub\" файл!");
-				} else {
-					lblLoadedFile.setText("Зареденият файл е : " + GlobalFile.getName());
-				}
-
-			}
-		});
-		toolBar.add(btnLoad);
-
-		JButton btnOpen = new JButton("\u041E\u0442\u0432\u043E\u0440\u0438 \u0424\u0430\u0439\u043B");
-		btnOpen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				// Frame1 frame1 = new Frame1();
-				JFileChooser chooser = new JFileChooser();
-				chooser.setDialogTitle("Open File");
-				chooser.showOpenDialog(null);
-				GlobalFile = chooser.getSelectedFile();
-				Frame1.openFrame();
-			}
-		});
-		toolBar.add(btnOpen);
-
-		JButton btnSave = new JButton("\u0417\u0430\u043F\u0438\u0448\u0438");
-		btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				JFileChooser fs = new JFileChooser();
-				fs.setDialogTitle("Save a File");
-				fs.setFileFilter(new FileTypeFilter(".srt", "Subtitle File"));
-				fs.setFileFilter(new FileTypeFilter(".sub", "Subtitle File"));
-				fs.showSaveDialog(null);
-				PrintStream fileWriter;
-				try {
-					fileWriter = new PrintStream(fs.getSelectedFile());
-					Scanner fr = new Scanner(temp);
-					while(fr.hasNextLine()){
-						fileWriter.println(fr.nextLine());
-					}
-
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-				}
-			}
-		});
-		toolBar.add(btnSave);
-
-		JButton btnExit = new JButton("\u0418\u0437\u0445\u043E\u0434");
-		btnExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-			}
-		});
-		toolBar.add(btnExit);
-
-		JRadioButton btnFaster = new JRadioButton("\u0417\u0430\u0431\u044A\u0440\u0437\u0430\u0439");
+		final JRadioButton btnFaster = new JRadioButton("\u0417\u0430\u0431\u044A\u0440\u0437\u0430\u0439");
+		btnFaster.setEnabled(false);
 		btnFaster.setBounds(10, 25, 94, 23);
 		frame.getContentPane().add(btnFaster);
 
-		JRadioButton btnSlower = new JRadioButton("\u0417\u0430\u0431\u0430\u0432\u0438");
+		final JRadioButton btnSlower = new JRadioButton("\u0417\u0430\u0431\u0430\u0432\u0438");
+		btnSlower.setEnabled(false);
 		btnSlower.setBounds(106, 25, 83, 23);
 		frame.getContentPane().add(btnSlower);
 
 		bg.add(btnFaster);
 		bg.add(btnSlower);
 
-		JCheckBox chckbxRemoveTags = new JCheckBox(
+		final JCheckBox chckbxRemoveTags = new JCheckBox(
 				"\u041F\u0440\u0435\u043C\u0430\u0445\u043D\u0438 \u0442\u0430\u0433\u043E\u0432\u0435\u0442\u0435");
+		chckbxRemoveTags.setEnabled(false);
 		chckbxRemoveTags.setBounds(286, 25, 142, 23);
 		frame.getContentPane().add(chckbxRemoveTags);
 
 		tfMlSeconds = new JTextField();
+		tfMlSeconds.setEditable(false);
 		tfMlSeconds.setBounds(193, 26, 50, 20);
 		frame.getContentPane().add(tfMlSeconds);
 		tfMlSeconds.setColumns(10);
@@ -196,19 +118,17 @@ public class Main {
 		label.setBounds(245, 29, 46, 14);
 		frame.getContentPane().add(label);
 
-		lblLoadedFile = new JLabel(
-				"\u0417\u0430\u0440\u0435\u0434\u0435\u043D\u0438\u044F\u0442 \u0444\u0430\u0439\u043B \u0435 ");
-		lblLoadedFile.setBounds(10, 128, 434, 14);
+		lblLoadedFile = new JLabel("");
+		lblLoadedFile.setBounds(10, 150, 434, 14);
 		frame.getContentPane().add(lblLoadedFile);
 
-		JLabel lbltime = new JLabel("00:01:09,028 \u0447. \u0434\u043E 00:01:10,321 \u0447.");
+		lbltime = new JLabel("");
 		lbltime.setOpaque(true);
 		lbltime.setHorizontalAlignment(SwingConstants.CENTER);
 		lbltime.setForeground(Color.WHITE);
 		lbltime.setBackground(Color.GRAY);
-		lbltime.setBounds(120, 83, 201, 35);
+		lbltime.setBounds(120, 107, 201, 35);
 		frame.getContentPane().add(lbltime);
-<<<<<<< HEAD
 		// Previous
 		final JButton btnPrevious = new JButton("\u041F\u0440\u0435\u0434\u0438\u0448\u0435\u043D");
 		btnPrevious.setEnabled(false);
@@ -263,25 +183,53 @@ public class Main {
 
 			}
 		});
-=======
->>>>>>> 5a65ed952c9790a8b769749f7741e092ce85b3da
 
-		tfScreen = new JTextField();
-		tfScreen.setBounds(0, 55, 444, 29);
-		frame.getContentPane().add(tfScreen);
-		tfScreen.setColumns(10);
+		JButton btnLoad = new JButton("\u0417\u0430\u0440\u0435\u0434\u0438 \u0424\u0430\u0439\u043B");
+		btnLoad.addActionListener(new ActionListener() {
 
-		JButton btnPrevious = new JButton("\u041F\u0440\u0435\u0434\u0438\u0448\u0435\u043D");
-		btnPrevious.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// Making browse window
+				JFileChooser chooser = new JFileChooser();
+				chooser.showOpenDialog(null);
+				// Check for subtitle file
+				if (chooser.getSelectedFile().getName().endsWith(".srt") == false
+						&& chooser.getSelectedFile().getName().endsWith(".sub") == false) {
+					JOptionPane.showMessageDialog(null, "Моля изберете \".srt или .sub\" файл!");
+				} else {
+					// Making file into raw content
+					GlobalFile = chooser.getSelectedFile();
+					subtitle = new Sub(GlobalFile);
+					entries = subtitle.entries();
+					lblLoadedFile.setText("Зареденият файл е : " + GlobalFile.getName());
+					epScreen.setEditable(true);
+					tfMlSeconds.setEditable(true);
+					btnFaster.setEnabled(true);
+					btnSlower.setEnabled(true);
+					btnNext.setEnabled(true);
+					btnPrevious.setEnabled(true);
+					chckbxRemoveTags.setEnabled(true);
+					// Parsing values from raw content to the UI
+					screen();
 
+				}
 			}
 		});
-		btnPrevious.setBounds(10, 94, 100, 23);
-		frame.getContentPane().add(btnPrevious);
+		toolBar.add(btnLoad);
+		toolBar.add(btnOpen);
 
-		JButton btnNext = new JButton("\u0421\u043B\u0435\u0434\u0432\u0430\u0449");
-		btnNext.setBounds(331, 94, 103, 23);
-		frame.getContentPane().add(btnNext);
+		JButton btnSave = new JButton("\u0417\u0430\u043F\u0438\u0448\u0438");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		toolBar.add(btnSave);
+
+		JButton btnExit = new JButton("\u0418\u0437\u0445\u043E\u0434");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		toolBar.add(btnExit);
 	}
 }
